dd85348b52cdd2ed6aa0167bf00dd6ee74bc3bb2
Navlok Mishra <navlokmishra@gmail.com>
2020-02-27 20:30:10 +0530

    Update commands.sh

--- commands.sh
+++ commands.sh
@@ -1,2 +1,7 @@
 # To check space occupied by a particular directory
 sudo ncdu -x <dir>
+
+# To set the shell to /bin/bash for an user
+# Motivation: I found that arrow, tab, ctrl+r etc were not working in Azure VM for non-admin users. 
+# It was because of the fact that the default shell for non-admin user in Azure VM was /bin/sh so had to change it to /bin/bash to make all those shortcut keys working
+sudo chsh -s /bin/bash <username> 
